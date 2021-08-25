c325bf189daded343e9eb4371e7dbb49483f6f36
pasindubhasura <srmpasindubhasura@gmail.com>
2021-08-26 00:41:39 +0530

    install vector icons

--- screens/Character.js
+++ screens/Character.js
@@ -1,10 +1,14 @@
 import React, {useState, useEffect} from 'react';
 import {View, Text} from 'react-native';
+import {Appbar} from 'react-native-paper';
 
 const Character = ({route, navigation}) => {
   const {id} = route.params;
   return (
     <View>
+      <Appbar.Header>
+        <Appbar.BackAction />
+      </Appbar.Header>
       <Text>{id}</Text>
     </View>
   );
