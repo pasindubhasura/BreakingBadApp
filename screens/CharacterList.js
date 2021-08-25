f1413c841514637dbe9c83f24db479bebbfd2380
pasindubhasura <srmpasindubhasura@gmail.com>
2021-08-25 22:05:35 +0530

    create fetch rquest to fetch data from the API

--- screens/CharacterList.js
+++ screens/CharacterList.js
@@ -3,9 +3,28 @@ import {View, Text} from 'react-native';
 import axios from 'axios';
 
 const CharacterList = () => {
+  const [characters, setCharacters] = useState([]); //state for characters data
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    console.log('characters');
+    try {
+      const response = await axios.get('https://www.breakingbadapi.com/api/characters');
+      setCharacters(response.data);
+      console.log('in characters');
+    } catch (error) {
+      console.log(error);
+    }
+  }; //sending api request to the endpoint
+
   return (
     <View>
-      <Text>CharacterList</Text>
+      {characters.map((i, index) => {
+        return <Text key={index}>{i.name}</Text>;
+      })}
     </View>
   );
 };
