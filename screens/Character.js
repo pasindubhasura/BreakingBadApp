390fdc868ad1338059aa73c0a0b9cd101f4d7159
pasindubhasura <srmpasindubhasura@gmail.com>
2021-08-25 17:24:12 +0530

    create screen folder

---
+++ screens/Character.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Character = () => {
+  return (
+    <View>
+      <Text>Character</Text>
+    </View>
+  );
+};
+
+export default Character;
---
+++ screens/CharacterList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const CharacterList = () => {
+  return (
+    <View>
+      <Text>CharacterList</Text>
+    </View>
+  );
+};
+
+export default CharacterList;
