e464c6bc38bbaeb18b5a271ecdfd5e14ddfb2837
pasindubhasura <srmpasindubhasura@gmail.com>
2021-08-26 13:32:03 +0530

    add back icon to app bar(did't work)

--- screens/Character.js
+++ screens/Character.js
@@ -1,15 +1,23 @@
 import React, {useState, useEffect} from 'react';
 import {View, Text} from 'react-native';
 import {Appbar} from 'react-native-paper';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
+const back = <FontAwesome5 name="chevron-left" solid />;
 const Character = ({route, navigation}) => {
   const {id} = route.params;
   return (
     <View>
       <Appbar.Header>
-        <Appbar.BackAction />
+        <Appbar.Action
+          onPress={() => {
+            navigation.popToTop();
+          }}
+          icon={<FontAwesome5 name={'chevron-left'} solid color={'white'} />}
+        />
       </Appbar.Header>
       <Text>{id}</Text>
+      <FontAwesome5 name={'chevron-left'} solid color={'white'} />
     </View>
   );
 };
