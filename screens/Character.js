1ef9d7c19fbc58f2c68038c1154e54ca1efbff41
pasindubhasura <srmpasindubhasura@gmail.com>
2021-08-26 13:38:10 +0530

    create fetchData method to send api request

--- screens/Character.js
+++ screens/Character.js
@@ -2,10 +2,29 @@ import React, {useState, useEffect} from 'react';
 import {View, Text} from 'react-native';
 import {Appbar} from 'react-native-paper';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import axios from 'axios';
 
 const back = <FontAwesome5 name="chevron-left" solid />;
 const Character = ({route, navigation}) => {
   const {id} = route.params;
+  const [character, setCharacter] = useState({});
+
+  useEffect(() => {
+    fetchData();
+  }, id);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        `https://www.breakingbadapi.com/api/characters/${id}`,
+      );
+      setCharacter(response.data[0]);
+      console.log('in characters');
+    } catch (error) {
+      console.log(error);
+    }
+  }; //sending api request to the endpoint
+
   return (
     <View>
       <Appbar.Header>
@@ -17,6 +36,7 @@ const Character = ({route, navigation}) => {
         />
       </Appbar.Header>
       <Text>{id}</Text>
+      <Text>{character.name}</Text>
       <FontAwesome5 name={'chevron-left'} solid color={'white'} />
     </View>
   );
