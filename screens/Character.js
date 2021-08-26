d972008f05f7c4df8bf4a1ff1e09595e402adf90
pasindubhasura <srmpasindubhasura@gmail.com>
2021-08-26 16:21:51 +0530

    fixed the issue with icons

--- screens/Character.js
+++ screens/Character.js
@@ -1,13 +1,15 @@
 import React, {useState, useEffect} from 'react';
 import {View, Text, StyleSheet, Image} from 'react-native';
 import {Appbar} from 'react-native-paper';
-import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+// import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import axios from 'axios';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
-const back = <FontAwesome5 name="chevron-left" solid />;
 const Character = ({route, navigation}) => {
   const {id} = route.params;
   const [character, setCharacter] = useState({});
+
+  const back = <FontAwesome name="chevron-left" color="red" size={40} />;
 
   useEffect(() => {
     fetchData();
@@ -27,14 +29,16 @@ const Character = ({route, navigation}) => {
 
   return (
     <View>
-      <Appbar.Header>
-        <Appbar.Content title={character.name} />
+      <Appbar.Header style={styles.appBar}>
         <Appbar.Action
           onPress={() => {
             navigation.popToTop();
           }}
-          icon={<FontAwesome5 name={'chevron-left'} solid color={'white'} />}
+          icon={() => {
+            return <FontAwesome name="chevron-left" color="white" size={24} />;
+          }}
         />
+        <Appbar.Content title={character.name} />
       </Appbar.Header>
       <View style={styles.card}>
         <View style={styles.imageContainer}>
@@ -55,7 +59,7 @@ const Character = ({route, navigation}) => {
 
 const styles = StyleSheet.create({
   card: {
-    backgroundColor: 'red',
+    backgroundColor: '#121212',
     width: '100%',
     height: '100%',
     alignItems: 'center',
@@ -71,6 +75,9 @@ const styles = StyleSheet.create({
   imageContainer: {
     width: '100%',
     height: '45%',
+  },
+  appBar: {
+    backgroundColor: '#121212',
   },
 });
 
